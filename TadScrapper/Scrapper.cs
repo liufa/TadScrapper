7425334508c5c0e37e14ebfacbd8e6154df2d2d6
liufa <mataswork@gmail.com>
2017-05-23 09:35:43 +0700

    Added Chrome driver

--- TadScrapper/Scrapper.cs
+++ TadScrapper/Scrapper.cs
@@ -1,17 +1,21 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TadScrapper
 {
     public class Scrapper : IDisposable
     {
+        public IWebDriver Driver;
 
         public Scrapper()
         {
-
+            this.Driver = new ChromeDriver();
         }
 
         public void Dispose()
